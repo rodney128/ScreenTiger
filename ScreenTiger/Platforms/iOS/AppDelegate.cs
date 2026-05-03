f7e7e5df9de14c57172dbbaa9e0c9bd5dc0edfff
rodney128 <contactrw@hotmail.com>
2026-05-02 17:02:46 -0700

    Initial commit

---
+++ ScreenTiger/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ScreenTiger
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
